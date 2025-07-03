0e5efaa377882c62ea5bbeb442d6e8a5760c8438
Sharookh-Codeedex <sharookh.codeedex@gmail.com>
2025-07-03 15:35:30 +0530

    Created admin order APIs

--- src/features/orders/orderRoute.js
+++ src/features/orders/orderRoute.js
@@ -11,16 +11,19 @@ const jwtVerify=require('../../middleware/jwtMiddleware')
 router.post('/confirm', jwtVerify(['user']), orderController.placeOrder)
 
 // Get orders for a user
-router.get('/user/:userId', jwtVerify(['user']), orderController.getUserOrders);
+router.get('/user/:userId', jwtVerify(['user','admin']), orderController.getUserOrders);
 
 // Get order details by ID
 router.get('/:orderId', jwtVerify(['user','admin']), orderController.getOrderById);
 
-// router.post('/invoice/:orderId', orderController.updateOrderStatus)
 
 router.post('/cancel/:orderId', jwtVerify(['user']), orderController.cancelOrder)
 
 router.post('/return/:orderId', jwtVerify(['user']), orderController.requestReturn)
+
+router.get('/', jwtVerify(['admin']), orderController.getAllOrders)
+
+router.patch('/update/:orderId', jwtVerify(['admin']), orderController.updateOrderStatus)
 
 
 
